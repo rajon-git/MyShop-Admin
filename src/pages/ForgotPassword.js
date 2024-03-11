b1c0e95f3623df3d60e8cd6a91371a637c6137d8
rajon-git <rajon.zhsust15@gmail.com>
2024-03-11 12:15:25 +0600

    login and reset password ui add

--- src/pages/ForgotPassword.js
+++ src/pages/ForgotPassword.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import CustomInput from '../components/CustomInput'
 
 function ForgotPassword() {
   return (
-    <div>ForgotPassword</div>
+    <div className='py-5' style={{background:"#ffd333", minHeight:"100vh"}}>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <div className='my-5 w-25 bg-white rounded-3 mx-auto p-4'>
+        <h3 className='text-center'>Forgot Password</h3>
+        <p className='text-center'>Please enter your registered email address for get reset token</p>
+        <form action=''>
+        <CustomInput type='text' label='Email Address' id='email'/>
+        <button className='border-0 px-3 py-2 text-white w-100 fw-bold' type='submit' style={{background:"#ffd333"}}>Submit</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import CustomInput from '../components/CustomInput'
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className='py-5' style={{background:"#ffd333", minHeight:"100vh"}}>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+     
+      <div className='my-5 w-25 bg-white rounded-3 mx-auto p-4'>
+        <h3 className='text-center'>Login</h3>
+        <p className='text-center'>Login to your account to continue</p>
+        <form action=''>
+        <CustomInput type='text' label='Email Address' id='email'/>
+        <CustomInput type='password' label='Password' id='pass'/>
+        <button className='border-0 px-3 py-2 text-white w-100 fw-bold' type='submit' style={{background:"#ffd333"}}>login</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
