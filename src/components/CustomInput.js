ad1d63909489f2ed051439a186506d3e08f0a774
rajon-git <rajon.zhsust15@gmail.com>
2024-03-15 16:08:30 +0600

    admin login api added

--- src/components/CustomInput.js
+++ src/components/CustomInput.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React from "react";
 
-function CustomInput(props) {
-    const {type,label,i_id,i_class} = props;
+const CustomInput = (props) => {
+  const { type, label, i_id, i_class, name, val, onChng, onBlr } = props;
   return (
-    <div className="form-floating mb-3">
-  <input type={type} className={`form-control ${i_class}`} id={i_id} placeholder={label}/>
-  <label htmlFor={label}>{label}</label>
-</div>
-  )
-}
+    <div className="form-floating mt-3">
+      <input
+        type={type}
+        className={`form-control ${i_class}`}
+        id={i_id}
+        placeholder={label}
+        name={name}
+        value={val}
+        onChange={onChng}
+        onBlur={onBlr}
+      />
+      <label htmlFor={label}>{label}</label>
+    </div>
+  );
+};
 
-export default CustomInput
+export default CustomInput;
--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,11 +1,13 @@
 import axios from "axios"
 import {base_url} from "../../utils/base_url"
 
-const login = async(userData)=>{
-    const response = await axios.post(`${base_url}user/admin-login`,userData);
-    console.log(response); 
-}
-
+const login = async (user) => {
+    const response = await axios.post(`${base_url}user/admin-login`, user);
+    if (response.data) {
+      localStorage.setItem("user", JSON.stringify(response.data));
+    }
+    return response.data;
+  };
 const authService = {
     login,
 }
