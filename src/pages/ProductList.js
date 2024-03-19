bed2e39fe1244b29bf2e714c1e60aa562bf3f548
rajon-git <rajon.zhsust15@gmail.com>
2024-03-19 12:38:20 +0600

    product lists add

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -1,43 +1,89 @@
-import React from 'react'
+import React, { useEffect } from "react";
 import { Table } from "antd";
-
+import { BiEdit } from "react-icons/bi";
+import { AiFillDelete } from "react-icons/ai";
+import { useDispatch, useSelector } from "react-redux";
+import { getProducts } from "../features/product/productSlice";
+import { Link } from "react-router-dom";
 const columns = [
-    {
-      title: "No.",
-      dataIndex: "key",
-    },
-    {
-      title: "Customer",
-      dataIndex: "name",
-    },
-    {
-      title: "Product",
-      dataIndex: "product",
-    },
-    {
-      title: "Status",
-      dataIndex: "status",
-    },
-  ];
+  {
+    title: "No",
+    dataIndex: "key",
+  },
+  {
+    title: "Image",
+    dataIndex: "images",
+    render: (images) => (
+      <img src={images[0]} alt="Product" style={{ width: 50, height: 50 }} />
+    ),
+  },
+  {
+    title: "Title",
+    dataIndex: "title",
+    sorter: (a, b) => a.title.length - b.title.length,
+  },
+  {
+    title: "Brand",
+    dataIndex: "brand",
+    sorter: (a, b) => a.brand.length - b.brand.length,
+  },
+  {
+    title: "Category",
+    dataIndex: "category",
+    sorter: (a, b) => a.category.length - b.category.length,
+  },
+  {
+    title: "Color",
+    dataIndex: "color",
+  },
+  {
+    title: "Price",
+    dataIndex: "price",
+    sorter: (a, b) => a.price - b.price,
+  },
+  {
+    title: "Action",
+    dataIndex: "action",
+  },
+];
+
+const Productlist = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getProducts());
+  }, []);
+  const productState = useSelector((state) => state.product.products);
   const data1 = [];
-  for (let i = 0; i < 46; i++) {
+  for (let i = 0; i < productState.length; i++) {
     data1.push({
-      key: i,
-      name: `Edward King ${i}`,
-      product: 32,
-      status: `London, Park Lane no. ${i}`,
+      key: i + 1,
+      images: productState[i].images,
+      title: productState[i].title,
+      brand: productState[i].brand,
+      category: productState[i].category,
+      color: productState[i].color,
+      price: `${productState[i].price}`,
+      action: (
+        <>
+          <Link to="/" className=" fs-3 text-danger">
+            <BiEdit />
+          </Link>
+          <Link className="ms-3 fs-3 text-danger" to="/">
+            <AiFillDelete />
+          </Link>
+        </>
+      ),
     });
   }
-
-function ProductList() {
+  console.log(data1);
   return (
     <div>
-      <h3 className='mb-4'>Products</h3>
+      <h3 className="mb-4 title">Products</h3>
       <div>
         <Table columns={columns} dataSource={data1} />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductList
+export default Productlist;
