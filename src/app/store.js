6b41293247a2df45ad16922629f5b962d54cb0b1
rajon-git <rajon.zhsust15@gmail.com>
2024-03-19 17:53:13 +0600

    blog slice service add

--- src/app/store.js
+++ src/app/store.js
@@ -4,6 +4,7 @@ import customerReducer from "../features/customers/customerSlice"
 import productReducer from "../features/product/productSlice"
 import brandReducer from "../features/brand/brandSlice"
 import pCategoryReducer from "../features/pcategory/pcategorySlice"
+import blogReducer from "../features/blogs/blogSlice"
 
 export const store = configureStore({
     reducer:
@@ -13,5 +14,6 @@ export const store = configureStore({
         product: productReducer,
         brand: brandReducer,
         pCategory: pCategoryReducer,
+        blog: blogReducer,
       }
 })
