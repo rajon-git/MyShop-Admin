fe967f7228777ad727d257c606d97d616bd8ff81
rajon-git <rajon.zhsust15@gmail.com>
2024-03-19 11:30:56 +0600

    customer list add

--- src/features/customers/customerService.js
+++ src/features/customers/customerService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { base_url } from "../../utils/base_url";
 
 const getUsers = async () => {
-  const response = await axios.post(`${base_url}user/all-users`);
+  const response = await axios.get(`${base_url}user/all-users`);
   return response.data;
 };
 const customerService = {
--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Table } from "antd";
+import { useDispatch, useSelector } from 'react-redux';
+import { getUsers } from '../features/customers/customerSlice';
 
 const columns = [
     {
@@ -7,29 +9,42 @@ const columns = [
       dataIndex: "key",
     },
     {
-      title: "Customer",
+      title: "Customer Name",
       dataIndex: "name",
+      defaultSortOrder: "descend",
+      sorter: (a,b)=> a.name.length - b.name.length
     },
     {
-      title: "Product",
-      dataIndex: "product",
+      title: "Email",
+      dataIndex: "email",
     },
     {
-      title: "Status",
-      dataIndex: "status",
+      title: "Mobile",
+      dataIndex: "mobile",
     },
   ];
-  const data1 = [];
-  for (let i = 0; i < 46; i++) {
-    data1.push({
-      key: i,
-      name: `Edward King ${i}`,
-      product: 32,
-      status: `London, Park Lane no. ${i}`,
-    });
-  }
+ 
 
 function Customers() {
+  const dispatch = useDispatch();
+  useEffect(()=>{
+    dispatch(getUsers())
+  },[]);
+  const customerState = useSelector((state)=> state.customer.customers);
+  const {data} = customerState;
+  const data1 = [];
+  for (let i = 0; i < customerState.length; i++) {
+    if(customerState[i].role !== 'admin')
+    {
+      data1.push({
+        key: i+1,
+        name: customerState[i].firstName +" "+customerState[i].lastName,
+        email: customerState[i].email,
+        mobile: customerState[i].mobile,
+      });
+    }
+  }
+  
   return (
     <div>
       <h3 className='mb-4'>Customers</h3>
