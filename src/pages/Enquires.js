0793778c410482ff5c94c35ff911a82593a5cd45
rajon-git <rajon.zhsust15@gmail.com>
2024-03-20 14:31:59 +0600

    enquiries slice, service and enquiries page implemented

--- src/pages/Enquires.js
+++ src/pages/Enquires.js
@@ -1,5 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Table } from "antd";
+import { useDispatch, useSelector } from "react-redux";
+import { BiEdit } from "react-icons/bi";
+import { AiFillDelete } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import { getEnquiries } from "../features/enquiry/enquirySlice";
 
 const columns = [
   {
@@ -7,29 +12,57 @@ const columns = [
     dataIndex: "key",
   },
   {
-    title: "Customer",
+    title: "Name",
     dataIndex: "name",
   },
   {
-    title: "Product",
-    dataIndex: "product",
+    title: "Email",
+    dataIndex: "email",
+  },
+  {
+    title: "Mobile",
+    dataIndex: "mobile",
   },
   {
     title: "Status",
     dataIndex: "status",
   },
+  {
+    title: "Action",
+    dataIndex: "action",
+  },
 ];
-const data1 = [];
-for (let i = 0; i < 46; i++) {
-  data1.push({
-    key: i,
-    name: `Edward King ${i}`,
-    product: 32,
-    status: `London, Park Lane no. ${i}`,
-  });
-}
+
 
 function Enquires() {
+  const dispatch = useDispatch();
+  useEffect(()=>{
+    dispatch(getEnquiries())
+  },[]);
+  const enquiryState = useSelector((state)=> state.enquiry.enquiries);
+  const data1 = [];
+  for (let i = 0; i < enquiryState.length; i++) {
+  data1.push({
+    key: i+1,
+    name: enquiryState[i].name,
+    email: enquiryState[i].email,
+    mobile: enquiryState[i].mobile,
+    status: (
+      <>
+        <select name="" className="form-control form-select" id="">
+          <option value="">Set Status</option>
+        </select>
+      </>
+    ),
+    action: (
+      <>
+        <Link className="ms-3 fs-3 text-danger" to="/">
+          <AiFillDelete />
+        </Link>
+      </>
+    ),
+  });
+}
   return (
     <div>
       <h3 className="mb-4">Enquires</h3>
