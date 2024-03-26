8706746e3cb62179e23da8f585fcfdccd24501f9
rajon-git <rajon.zhsust15@gmail.com>
2024-03-26 22:34:41 +0600

    update

--- src/pages/BrandList.js
+++ src/pages/BrandList.js
@@ -26,15 +26,15 @@ const columns = [
 function BrandList() {
   const dispatch = useDispatch();
   const [open, setOpen] = useState(false);
-  const [brandId, setBrandId] = useState("")
+  const [brandId, setBrandId] = useState("");
   const showModal = (e) => {
     setOpen(true);
-    setBrandId(e)
+    setBrandId(e);
   };
   const hideModal = () => {
     setOpen(false);
   };
-  
+
   useEffect(() => {
     dispatch(getBrands());
   }, []);
@@ -42,14 +42,20 @@ function BrandList() {
   const data1 = [];
   for (let i = 0; i < brandState.length; i++) {
     data1.push({
-      key: i+1,
+      key: i + 1,
       name: brandState[i].title,
       action: (
         <>
-          <Link to={`/admin/brand/${brandState[i]._id}`} className=" fs-3 text-danger">
+          <Link
+            to={`/admin/brand/${brandState[i]._id}`}
+            className=" fs-3 text-danger"
+          >
             <BiEdit />
           </Link>
-          <button className="ms-3 fs-3 text-danger bg-transparent border-0"onClick={()=> showModal(brandState[i]._id)}>
+          <button
+            className="ms-3 fs-3 text-danger bg-transparent border-0"
+            onClick={() => showModal(brandState[i]._id)}
+          >
             <AiFillDelete />
           </button>
         </>
@@ -57,22 +63,27 @@ function BrandList() {
     });
   }
 
-  const deleteBrand = (e)=>{
-    
+  const deleteBrand = (e) => {
     dispatch(deleteABrand(e));
     setOpen(false);
-    setTimeout(()=>{
+    setTimeout(() => {
       dispatch(getBrands());
     }, 100);
-    
-  }
+  };
   return (
     <div>
       <h3 className="mb-4">Brand List</h3>
       <div>
         <Table columns={columns} dataSource={data1} />
       </div>
-      <CustomModal hideModal={hideModal} open={open} performAction={()=>{deleteBrand(brandId)}} title='Are you sure, want to delete this brand?'/>
+      <CustomModal
+        hideModal={hideModal}
+        open={open}
+        performAction={() => {
+          deleteBrand(brandId);
+        }}
+        title="Are you sure, want to delete this brand?"
+      />
     </div>
   );
 }
