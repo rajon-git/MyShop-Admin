2310a8fbc21e1a3fbc10c23a59b6171540855e5e
rajon-git <rajon.zhsust15@gmail.com>
2024-03-27 23:46:58 +0600

    get, update , delete product category added

--- src/features/pcategory/pcategoryService.js
+++ src/features/pcategory/pcategoryService.js
@@ -13,9 +13,33 @@ const getProductCategories = async () => {
   return response.data;
 };
 
+const getProductCategory = async (id) => {
+  const response = await axios.get(`${base_url}category/${id}`, config);
+
+  return response.data;
+};
+
+const deleteProductCategory = async (id) => {
+  const response = await axios.delete(`${base_url}category/${id}`, config);
+
+  return response.data;
+};
+
+const updateProductCategory = async (category) => {
+  const response = await axios.put(
+    `${base_url}category/${category.id}`,
+    { title: category.categoryData.title },
+    config
+  );
+  return response.data;
+}
+
 const pCategoryService = {
   getProductCategories,
-  createCategory
+  createCategory,
+  getProductCategory,
+  deleteProductCategory,
+  updateProductCategory
 };
 
 export default pCategoryService;
