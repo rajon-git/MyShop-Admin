dd9fb83a30aed05ed6f6890e4691c78beeb35944
rajon-git <rajon.zhsust15@gmail.com>
2024-03-31 23:17:16 +0600

    enquiry list page implemented

--- src/pages/Enquires.js
+++ src/pages/Enquires.js
@@ -1,10 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Table } from "antd";
 import { useDispatch, useSelector } from "react-redux";
 import { BiEdit } from "react-icons/bi";
 import { AiFillDelete } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import { getEnquiries } from "../features/enquiry/enquirySlice";
+import { FaRegEye } from "react-icons/fa6";
+import CustomModal from "../components/CustomModal";
 
 const columns = [
   {
@@ -36,6 +38,15 @@ const columns = [
 
 function Enquires() {
   const dispatch = useDispatch();
+  const [open, setOpen] = useState(false);
+  const [enqId, setEnqId] = useState("");
+  const showModal = (e) => {
+    setOpen(true);
+    setEnqId(e);
+  };
+  const hideModal = () => {
+    setOpen(false);
+  };
   useEffect(()=>{
     dispatch(getEnquiries())
   },[]);
@@ -56,19 +67,40 @@ function Enquires() {
     ),
     action: (
       <>
+        <button
+            className="ms-3 fs-3 text-danger bg-transparent border-0"
+            onClick={() => showModal(enquiryState[i]._id)}
+          >
+            <AiFillDelete />
+          </button>
         <Link className="ms-3 fs-3 text-danger" to="/">
-          <AiFillDelete />
+          <FaRegEye />
         </Link>
       </>
     ),
   });
 }
+const deleteEnq = (e) => {
+  dispatch(deleteAColor(e));
+  setOpen(false);
+  setTimeout(() => {
+    dispatch(getEnquiries());
+  }, 500);
+};
   return (
     <div>
       <h3 className="mb-4">Enquires</h3>
       <div>
         <Table columns={columns} dataSource={data1} />
       </div>
+      <CustomModal
+        hideModal={hideModal}
+        open={open}
+        performAction={() => {
+          deleteEnq(enqId);
+        }}
+        title="Are you sure, want to delete this enquiry?"
+      />
     </div>
   );
 }
--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,8 +1,22 @@
-const token = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')).token : null;
+// const token = localStorage.getItem("user")
+//   ? JSON.parse(localStorage.getItem("user")).token
+//   : null;
 
+// export const config = {
+//   headers: {
+//     Authorization: `Bearer ${token}`,
+//     Accept: "application/json",
+//   },
+// };
+
+const getTokenFromLocalStorage = localStorage.getItem("user")
+  ? JSON.parse(localStorage.getItem("user"))
+  : null;
 export const config = {
-        headers: {
-            Authorization: `Bearer ${token}`,
-            Accept: "application/json",
-        }
-    };
+  headers: {
+    Authorization: `Bearer ${
+      getTokenFromLocalStorage !== null ? getTokenFromLocalStorage.token : ""
+    }`,
+    Accept: "application/json",
+  },
+};
